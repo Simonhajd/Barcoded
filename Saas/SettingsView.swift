e07515ae598dfff544aada89ccd1f52a914ab785
Simon Hajduk <simonhajduk@icloud.com>
2024-07-03 10:35:54 -0600

    Added a template settings page

---
+++ Saas/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Saas
+//
+//  Created by Simon Hajduk on 7/3/24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
